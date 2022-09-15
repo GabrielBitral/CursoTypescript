deac019c29a6fc4e92cc45b14663aecc40adae3c
Gabriel Fabiano <77023882+GabrielBitral@users.noreply.github.com>
2022-09-14 22:03:09 -0300

    Alterações

--- Parte 1/app/src/app.ts
+++ Parte 1/app/src/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 if (form) {
@@ -6,7 +7,16 @@ if (form) {
         event.preventDefault();
         controller.adiciona();
     });
-}
-else {
+} else {
     throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
 }
+
+const botaoImporta = document.querySelector('#botao-importa');
+if (botaoImporta) {
+    botaoImporta.addEventListener('click', () => {
+        controller.importaDados()
+    });
+} else {
+    throw Error('Não foi possível encontrar o botão importa.');
+}
+
